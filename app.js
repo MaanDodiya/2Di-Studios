13d7ecb554e1fded8db5e8a4263cd0e7ed23fdc6
Maan Dodiya <maand94@gmail.com>
2021-01-04 20:27:37 +0530

    Renaissance Album added

---
+++ app.js
@@ -0,0 +1,9 @@
+var URL = 'https://raw.githubusercontent.com/MaanDodiya/2di-studios/main/data.json'
+	
+async function getJSONData (url) {
+	const response = await fetch(url);
+	var data = await response.json();
+	console.log(data)
+}
+
+getJSONData(URL);
