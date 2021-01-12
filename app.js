2426faddd75c55a01ebb045fb1a1fde5d71f550f
Maan Dodiya <maand94@gmail.com>
2021-01-12 15:47:26 +0530

    Album column for 1 done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Down comes the list of albums and details
 const data = {
 	"Renaissance": {
-		"name": "Renaissance",
+		"name": "renaissance",
 		"date": {
 			"day": "2",
 			"month": "1",
@@ -16,3 +16,21 @@ const data = {
 
 // Code starts
 
+function purchase() {
+	alert("Working!")
+}
+
+function changeCover(album) {
+	var image = document.querySelector("section#"+album+" img");
+	var button = document.querySelector("section#"+album+" .info #buttons #view")
+	if(image.className=="front") {
+		image.className="back";
+		image.src = "./images/"+album+"/backCover.png"
+		button.innerHTML = "View Front Cover"
+	}
+	else{
+		image.className="front";
+		image.src = "./images/"+album+"/frontCover.png"
+		button.innerHTML = "View Back Cover"
+	}		
+}
