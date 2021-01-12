c54d143757b7dc5e7dedda79543b71592170e0f4
Maan Dodiya <maand94@gmail.com>
2021-01-12 12:53:46 +0530

    Website Boilerplate

---
+++ app.js
@@ -0,0 +1,18 @@
+// Down comes the list of albums and details
+const data = {
+	"Renaissance": {
+		"name": "Renaissance",
+		"date": {
+			"day": "2",
+			"month": "1",
+			"year": "21"
+		},
+		"price": "6.69",
+		"description": " ",
+		"backCover": "./images/renaissance/backCover.png",
+		"frontCover": "./images/renaissance/frontCover.png"
+	}
+}
+
+// Code starts
+
